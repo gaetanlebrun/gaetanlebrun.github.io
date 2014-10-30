25e845fe8593ef90655662e01e21af7b6d44f1bc
Gaetan Le Brun <gaetan.lebrun@gmail.com>
2014-10-30 15:44:24 +0000

    add .nojekyll to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -324,6 +324,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
+			'.nojekyll',
             'styles/fonts/{,*/}*.*'
           ]
         }, {
